bfc34fd2a909cdda21f971c21919e0a0f4138d83
Flow2510 <sendra.florian@gmail.com>
2025-06-02 12:41:38 +0200

    ajout d'un fichier functions.js pour separer les fonctions, css pour le modal

---
+++ FrontEnd/assets/functions.js
@@ -0,0 +1,123 @@
+let allWorks = [];     // creation d'une "sauvegarde" pour utiliser le tableau globalement
+
+export async function loadFilters(){
+    const url = "http://localhost:5678/api/categories";
+    const response = await fetch(url);
+    const categories = await response.json();
+    const gallery = document.querySelector('.gallery');
+    const portfolio = document.querySelector('#portfolio');
+    const div = document.createElement('div');
+    
+    div.classList.add('filters_container');
+    portfolio.insertBefore(div, gallery)
+
+    const allButton = document.createElement('button');
+    allButton.innerText = 'Tous';
+    allButton.classList.add('all_filter');
+    allButton.classList.add('selected');
+    div.appendChild(allButton);
+    
+    categories.forEach(categorie => {
+        const button = document.createElement('button');
+
+        button.classList.add('filter');
+        button.dataset.id = categorie.id;
+        button.innerText = categorie.name;
+        div.appendChild(button);
+    });
+
+    const filters = document.querySelectorAll('.filter')
+
+    allButton.addEventListener('click', () => {
+        filters.forEach(f => {
+            f.classList.remove('selected');   
+        })
+        allButton.classList.add('selected');
+        gallery.innerHTML = "";
+        showProjects();
+    })
+
+    filters.forEach(filter => {        
+        filter.addEventListener('click', () => {
+            filters.forEach(f => {
+                f.classList.remove('selected');   
+            })
+            allButton.classList.remove('selected');
+            gallery.innerHTML = "";
+            filter.classList.add('selected');
+
+            const categoryId = parseInt(filter.dataset.id); // Ne marche pas sans parseInt, CONVERTIT LA CHAINE EN NOMBRE
+            const filteredWorks = allWorks.filter(work => work.categoryId === categoryId); //pour chaque work, on verifie si il a la bonne categorie, si oui on le met dans le nouveau tableau
+
+            showProjects(filteredWorks);
+        })
+    })
+    // faire apparaitre un bouton par categorie                                                     OK
+    // ajouter un bouton pour "tous"                                                                OK
+    // changer la couleur du bouton du filtre selectionné                                           OK
+    // faire un add event sur les boutons au clique, avec showProjects                              OK
+    // faire en sorte que showProjects prenne en compte le parametre de l'id du bouton              OK
+}
+
+export async function loadProjects() {      // fonction pour charger les projets au demarrage
+    const url = "http://localhost:5678/api/works"
+    const response = await fetch(url);
+    const works = await response.json();
+    
+    allWorks = works; // 'sauvegarde' dans la variable pour etre en portee globale
+    showProjects();
+}
+
+export function showProjects(works = allWorks){
+    for (let work of works) {
+        const gallery = document.querySelector('.gallery');
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        const figCaption = document.createElement('figcaption')
+
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figCaption.innerText = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        gallery.appendChild(figure);
+    }
+}
+
+export async function showModalImg () {
+    const url = "http://localhost:5678/api/works"
+    const response = await fetch(url);
+    const works = await response.json();
+
+    for (let work of works) {
+        const modalGallery = document.querySelector('.modal__gallery');
+        const div = document.createElement('div');
+        div.classList.add('gallery__wrapper');
+        div.innerHTML = `<i class="gallery__icon fa-solid fa-trash-can"></i>`;
+        div.style.background = `url("${work.imageUrl}")`;  // bien mettre ${} sinon affiche que la premiere image avec work.imageUrl
+        div.style.backgroundSize = "cover";
+        div.style.backgroundPosition = 'center';
+        modalGallery.appendChild(div);
+        
+        // galleryIcon = document.querySelector('.gallery__icon');
+        // galleryIcon.addEventListener('click', () => {
+        //         est ce que je peux supprimer sans supprimer definitivement ? a voir avec mentor
+        // })
+    }
+}
+
+export async function showOptions() {
+    const url = "http://localhost:5678/api/categories";
+    const response = await fetch(url);
+    const categories = await response.json();
+    const select = document.querySelector('.add__select');
+
+    select.innerHTML = "";
+
+    for (let categorie of categories) {
+        const option = document.createElement('option');
+        option.classList.add('add__select-option');
+        option.innerText = `${categorie.name}`;
+        select.appendChild(option);
+    }
+}
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,113 +1,4 @@
-let allWorks = [];     // creation d'une "sauvegarde" pour utiliser le tableau globalement
-
-
-async function loadFilters(){
-    const url = "http://localhost:5678/api/categories";
-    const response = await fetch(url);
-    const categories = await response.json();
-    const gallery = document.querySelector('.gallery');
-    const portfolio = document.querySelector('#portfolio');
-    const div = document.createElement('div');
-    
-    div.classList.add('filters_container');
-    portfolio.insertBefore(div, gallery)
-
-    const allButton = document.createElement('button');
-    allButton.innerText = 'Tous';
-    allButton.classList.add('all_filter');
-    allButton.classList.add('selected');
-    div.appendChild(allButton);
-    
-    categories.forEach(categorie => {
-        const button = document.createElement('button');
-
-        button.classList.add('filter');
-        button.dataset.id = categorie.id;
-        button.innerText = categorie.name;
-        div.appendChild(button);
-    });
-
-    const filters = document.querySelectorAll('.filter')
-
-    allButton.addEventListener('click', () => {
-        filters.forEach(f => {
-            f.classList.remove('selected');   
-        })
-        allButton.classList.add('selected');
-        gallery.innerHTML = "";
-        showProjects();
-    })
-
-    filters.forEach(filter => {        
-        filter.addEventListener('click', () => {
-            filters.forEach(f => {
-                f.classList.remove('selected');   
-            })
-            allButton.classList.remove('selected');
-            gallery.innerHTML = "";
-            filter.classList.add('selected');
-
-            const categoryId = parseInt(filter.dataset.id); // Ne marche pas sans parseInt, CONVERTIT LA CHAINE EN NOMBRE
-            const filteredWorks = allWorks.filter(work => work.categoryId === categoryId); //pour chaque work, on verifie si il a la bonne categorie, si oui on le met dans le nouveau tableau
-
-            showProjects(filteredWorks);
-        })
-    })
-    
-
-    // faire apparaitre un bouton par categorie                                                     OK
-    // ajouter un bouton pour "tous"                                                                OK
-    // changer la couleur du bouton du filtre selectionné                                           OK
-    // faire un add event sur les boutons au clique, avec showProjects                              OK
-    // faire en sorte que showProjects prenne en compte le parametre de l'id du bouton              OK
-}
-
-async function loadProjects() {      // fonction pour charger les projets au demarrage
-    const url = "http://localhost:5678/api/works"
-    const response = await fetch(url);
-    const works = await response.json();
-    
-    allWorks = works; // 'sauvegarde' dans la variable pour etre en portee globale
-    showProjects();
-}
-
-function showProjects(works = allWorks){
-    for (let work of works) {
-        const gallery = document.querySelector('.gallery');
-        const figure = document.createElement('figure');
-        const img = document.createElement('img');
-        const figCaption = document.createElement('figcaption')
-
-        img.src = work.imageUrl;
-        img.alt = work.title;
-        figCaption.innerText = work.title;
-        figure.appendChild(img);
-        figure.appendChild(figCaption);
-        gallery.appendChild(figure);
-    }
-}
-
-async function showModalImg () {
-    const url = "http://localhost:5678/api/works"
-    const response = await fetch(url);
-    const works = await response.json();
-
-    for (let work of works) {
-        const modalGallery = document.querySelector('.modal__gallery');
-        const div = document.createElement('div');
-        div.classList.add('gallery__wrapper');
-        div.innerHTML = `<i class="gallery__icon fa-solid fa-trash-can"></i>`;
-        div.style.background = `url("${work.imageUrl}")`;  // bien mettre ${} sinon affiche que la premiere image avec work.imageUrl
-        div.style.backgroundSize = "cover";
-        div.style.backgroundPosition = 'center';
-        modalGallery.appendChild(div);
-        
-        // galleryIcon = document.querySelector('.gallery__icon');
-        // galleryIcon.addEventListener('click', () => {
-        //         est ce que je peux supprimer sans supprimer definitivement ? a voir avec mentor
-        // })
-    }
-}
+import * as funcs from './functions.js';
 
 const token = localStorage.getItem("token");
 
@@ -137,10 +28,11 @@ if (token) {
 const gallery = document.querySelector('.gallery');
 
 if (gallery){
-    loadFilters().then(() => {  // on attend que loadFilters a fini de charger sinon loadProject charge casi en meme temps et ca bug
-        loadProjects().then(() => {
+    funcs.loadFilters().then(() => {  // on attend que loadFilters a fini de charger sinon loadProject charge casi en meme temps et ca bug
+        funcs.loadProjects().then(() => {
             if (token){
                 document.querySelector('.portfolio__modify-wrapper').style.display = "flex";
+                document.querySelector('.header__modify-wrapper').style.display = "flex";
             }
         });
     });
@@ -148,7 +40,7 @@ if (gallery){
 
 document.querySelector('.portfolio__modify-wrapper').addEventListener('click', () => {
     document.querySelector('.modal__wrapper').style.display = "flex";
-    showModalImg();
+    funcs.showModalImg();
 })
 
 document.querySelector('.modal__icon-close').addEventListener('click', () => {
@@ -159,13 +51,14 @@ document.querySelector('.modal__icon-close').addEventListener('click', () => {
 document.querySelector('.modal__button').addEventListener('click', () => {
     document.querySelector('.add__section').style.display = 'block';
     document.querySelector('.modal__section').style.display = 'none';
+    funcs.showOptions();
 })
 
-document.querySelector('.modal__icon-return').addEventListener('click', () => {
+document.querySelector('.add__icon-return').addEventListener('click', () => {
     document.querySelector('.add__section').style.display = "none";
-    document.querySelector('.modal__section').style.display = "flex";
+    document.querySelector('.modal__section').style.display = "block";
     document.querySelector('.modal__gallery').innerHTML = "";
-    showModalImg();
+    funcs.showModalImg();
 })
 
 document.querySelector('.add__icon-close').addEventListener('click', () => {
